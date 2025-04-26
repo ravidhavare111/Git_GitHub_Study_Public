78ee2e8355fd6f32a0016822e164961a54d3b901
Ravi Dhavare <100593775+ravidhavare111@users.noreply.github.com>
2025-04-26 18:15:12 +0530

    Initial commit

---
+++ Demo/DemoForGit.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class DemoForGit {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("This is a demo Java program.");
+        System.out.println("Let's learn Java together!");
+        System.out.println("This is a simple Java program to demonstrate basic syntax.");
+    }
+    
+}
