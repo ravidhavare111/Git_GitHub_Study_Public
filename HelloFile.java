19904631a105cbbb53e136429d005c143ce8f83e
Ravi Dhavare <100593775+ravidhavare111@users.noreply.github.com>
2025-04-26 18:21:49 +0530

    add 2 new methods in HelloFile.java. 1.getName() and 2. printName()

--- HelloFile.java
+++ HelloFile.java
@@ -2,4 +2,11 @@ public class HelloFile {
         public static void main(String[] args) {
             System.out.println("Hello All");
         }
+
+        public String getName() {
+            return "HelloFile";
+        }
+        public void printName() {
+            System.out.println(getName());
+        }
 }
